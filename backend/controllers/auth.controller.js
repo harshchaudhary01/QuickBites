858439d6ab2a82befea7af0601d9b4e48dc67572
harshchaudhary01 <harshcareer01@gmail.com>
2026-02-07 14:01:21 +0530

    Building Auth Controllers for SignUp

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,43 @@
+import User from "../models/user.model.js";
+import bcrypt from "bcrypt.js";
+import generateToken from "../utils/token.js";
+
+export const signUp = async (req, res) =>{
+    try {
+        const {userName, email, password, mobile, role} = req.body;
+        const user = await User.findOne({email});
+        if(user){
+            return res.status(400).json({message: "User already exist."}); // we use "400", bcoz it's the fault of client side user.
+        }
+        if(password.length < 6){
+            return res.status(400).json({message: "Password must be atleast 6 characters long."});
+        }
+        if(mobile.length < 10){
+            return res.status(400).json({message: "Mobile number must be 10 digits long."});
+        }
+
+        const hashedPassword = await bcrypt.hash(password,10);
+        user = await User.create({
+            fullName,
+            email,
+            password: hashedPassword,
+            mobile,
+            role
+        })
+
+        // now, user is created, the first thing we have to do after this, is to generate the token 
+
+        const token = await generateToken(user._id);
+        res.cookie("token", token, {
+            secure: false, // in the dev phase, we use "false" because we only uses "http" & secure means "https"...
+            sameSite: "strict", // whenever, we put secure = false, we also put sameSite = "strict"
+            maxAge: 7*24*60*60*1000, // we give the value in millisecond not like "7d"
+            httpOnly: true,
+        })
+
+        return res.status(201).json(user);
+
+    } catch (error) {
+        return res.status(201).json(`SignUp Error: ${error}`);
+    }
+}
---
+++ backend/utils/token.js
@@ -0,0 +1,11 @@
+import jwt from "jsonwebtoken";
+
+const generateToken = async (userId)=>{
+    try {
+        const token = jwt.sign({userId},process.env.JWT_SECRET,{expiresIn: "7d"});
+        return token;
+    } catch (error) {
+        console.log(`Token Generate Error: ${error}`);
+    }
+}
+export default generateToken;
