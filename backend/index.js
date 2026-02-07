dc83bd4b33e1f9ffbf0087024dea9801acd910c0
harshchaudhary01 <harshcareer01@gmail.com>
2026-02-07 16:21:58 +0530

    Adding necessary middlewares inside the app.js

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
-import connectDB from "./config/db.js";
-
 dotenv.config();
-const port = process.env.PORT || 5000;
+import connectDB from "./config/db.js";
+import cookieParser from "cookie-parser";
+import authRouter from "./routes/authRoute.js"
 const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    Credentials: true
+}))
+
+app.use(express.json());
+app.use(cookieParser());
+app.use("/api/auth",authRouter);
 
 app.listen(port, ()=>{
     connectDB();
