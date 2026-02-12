7a1772fb7a344acbf2cf705a37131d04ad19577a
harshchaudhary01 <harshcareer01@gmail.com>
2026-02-12 11:38:30 +0530

    Building Auth Middleware

---
+++ backend/middlewares/isAuth.js
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken";
+
+const isAuth = async (req, res, next) => {
+    try {
+        const token = req.cookie.token;
+        if (!token) {
+            return res.status(400).json({ message: `Token not found!` })
+        }
+        const decodeToken = jwt.verify(token, process.env.JWT_SECRET);
+        if (!decodeToken) {
+            return res.status(400).json({ message: `Token not verified!` })
+        }
+        console.log(decodeToken)
+        req.userId = decodeToken.userId;
+        next();
+    } catch (error) {
+        res.status(500).json({message: `auth middleware error: ${error}`});
+    }
+}
