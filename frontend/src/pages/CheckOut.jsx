e108cda7f9e5421d03d2c27ec5fd4ed8d85df952
harshchaudhary01 <harshcareer01@gmail.com>
2026-03-29 20:10:29 +0530

    fix: current location fetching issue on map

--- frontend/src/pages/CheckOut.jsx
+++ frontend/src/pages/CheckOut.jsx
@@ -42,7 +42,7 @@ const CheckOut = () => {
     const navigate = useNavigate();
 
     const { location, address } = useSelector(state => state.map);
-    const { cartItems, totalAmount } = useSelector(state => state.user);
+    const { cartItems, totalAmount, userData } = useSelector(state => state.user);
 
     const deliveryFee = (totalAmount > 500) ? 0 : 49;
     const amountWithDeliveryFee = totalAmount + deliveryFee;
@@ -50,12 +50,10 @@ const CheckOut = () => {
 
 
     const getCurrentLocation = () => {
-        navigator.geolocation.getCurrentPosition(async (position) => {
-            const latitude = position.coords.latitude;
-            const longitude = position.coords.longitude;
-            dispatch(setLocation({ lat: latitude, lon: longitude }));
-            getAddressByLatLng(latitude, longitude);
-        });
+        const latitude = userData.location.coordinates[1];
+        const longitude = userData.location.coordinates[0];
+        dispatch(setLocation({ lat: latitude, lon: longitude }));
+        getAddressByLatLng(latitude, longitude);
     };
 
     const getAddressByLatLng = async (lat, lng) => {
